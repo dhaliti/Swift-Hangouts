951b47e110a8a4cda59964f79eb0f78beca58080
dhaliti <damirhaliti@icloud.com>
2022-10-17 02:41:05 +0200

    Init

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import AppNavigator from './Navigation/Navigator';
+import 'react-native-gesture-handler';
+import { StyleSheet, View } from "react-native";
+import {useColorScheme} from 'react-native';
+import {DarkTheme,DefaultTheme,NavigationContainer} from '@react-navigation/native';
+
+
+const App = () => {
+
+  return <AppNavigator />;
+
+};
+
+const style = StyleSheet.create( {
+  general: {
+    backgroundColor: '#dcdcdc'
+  }
+})
+export default App;
---
+++ Navigation/Navigator.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import {NavigationContainer, DefaultTheme, DarkTheme,} from '@react-navigation/native';
+import HomeScreen from './Screens/HomeScreen';
+import AddContactScreen from './Screens/AddContactScreen';
+import ContactDetailsScreen from './Screens/ContactDetailsScreen';
+import SettingsScreen from './Screens/SettingsScreen';
+import EditContactScreen from "./Screens/EditContactScreen";
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { View, Text, StyleSheet } from "react-native";
+
+const {Navigator, Screen} = createNativeStackNavigator();
+
+const AppNavigator = () => {
+  return (
+      <NavigationContainer theme={DarkTheme} >
+        <Navigator screenOptions={{headerShown: false}} initialRouteName={"Home"}>
+          <Screen name="Home" component={HomeScreen} />
+          <Screen name="Settings" component={SettingsScreen} />
+          <Screen name="ContactDetails" component={ContactDetailsScreen} />
+          <Screen name="AddContactScreen" component={AddContactScreen} />
+          <Screen name="EditContactScreen" component={EditContactScreen} />
+        </Navigator>
+      </NavigationContainer>
+  );
+};
+
+
+export default AppNavigator;
